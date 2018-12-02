30375344c822b8bc783dccdc4f97105bba1255c9
student <student@localhost.com>
2018-12-02 16:18:13 +0300

    Coverage requirements met

--- src/test/java/ru/itpark/service/CashBackServiceTest.java
+++ src/test/java/ru/itpark/service/CashBackServiceTest.java
@@ -1,5 +1,6 @@
 package ru.itpark.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashBackServiceTest {
 
     @Test
-    void cashback() {
+    @DisplayName("should calculate cashback under limit amount")
+    void cashbackUnderLimit() {
+        CashBackService service = new CashBackService();
+        int result = service.cashback(1_000);
+        assertEquals(50, result);
     }
 }
