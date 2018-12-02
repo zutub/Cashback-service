8eaeb129d7ebe2463649f043a98f494ff87656f2
student <student@localhost.com>
2018-12-02 15:46:23 +0300

    Initial commit

---
+++ src/main/java/ru/itpark/service/CashBackService.java
@@ -0,0 +1,7 @@
+package ru.itpark.service;
+
+public class CashBackService {
+    public int cashback(int amount) {
+        return amount * 5 / 100;
+    }
+}
---
+++ src/test/java/ru/itpark/service/CashBackServiceTest.java
@@ -0,0 +1,12 @@
+package ru.itpark.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashBackServiceTest {
+
+    @Test
+    void cashback() {
+    }
+}
